fa95f4a110a14be8d21657c49d871fa44f25871a
Leandro Parrado <lparrado321@gmail.com>
2021-01-30 11:19:05 -0500

    Added comments in seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+-- Still in the db folder, create a seeds.sql file. In this file, write insert queries to populate the burgers table with at least three entries.
+-- Run the schema.sql and seeds.sql files into the mysql server from the command line
