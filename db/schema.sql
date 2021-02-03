358552dd1e932e7cee5cdf0d9ccbde6c4df9db6c
Leandro Parrado <lparrado321@gmail.com>
2021-02-02 23:12:08 -0500

    Changed cats to burgers and sleepy to devoured.

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,11 @@ DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 CREATE TABLE burgers (
+  -- burgers_name VARCHAR(30) UNIQUE NOT NULL,
+  -- devoured VARCHAR(30) UNIQUE NOT NULL  
+
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  burgers_name VARCHAR(30) UNIQUE NOT NULL,
-  devoured VARCHAR(30) UNIQUE NOT NULL  
+	name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false
+	-- PRIMARY KEY (id)
 );
